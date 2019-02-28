68eb78ff5aa6e6c17f595d643d9a5dcbcfc7724b
Srdjan Pejic <spejic@gmail.com>
2019-02-28 00:16:30 -0500

    Added a form that lets the user select the sort direction and the beginning of the pagination scheme.

--- lib/links_web/controllers/link_controller.ex
+++ lib/links_web/controllers/link_controller.ex
@@ -4,7 +4,11 @@ defmodule LinksWeb.LinkController do
   alias ParttransEngine.LinkDisplayer
 
   def index(conn, params) do
-    atom_params = for {key, val} <- params, into: %{}, do: {String.to_existing_atom(key), val}
+    atom_params = for {key, val} <- params, key_in_whitelist?(key), into: %{}, do: {String.to_existing_atom(key), val}
     render conn, "index.html", links: Enum.chunk_every(LinkDisplayer.to_list(atom_params), 3)
+  end
+
+  defp key_in_whitelist?(key) do
+    Enum.any?(["page", "per_page", "sort_direction", "archived"], fn(i) -> i == key end)
   end
 end
