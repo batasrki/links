2c4ba5e261a05fa3eb7d3cc5ccd81b68174d4c30
Srdjan Pejic <spejic@gmail.com>
2019-07-24 20:34:35 -0400

    Version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Links.Mixfile do
   def project do
     [
       app: :links,
-      version: "0.3.3",
+      version: "0.3.4",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
