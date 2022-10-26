e76df24be843c17856d14acba3c1ccd7c1f27577
Srdjan Pejic <spejic@gmail.com>
2022-10-26 19:17:23 -0400

    Changing mail API keys

--- config/releases.exs
+++ config/releases.exs
@@ -1,3 +1,4 @@
 import Config
 config :links, :login_request_salt, System.fetch_env!("SECRET_KEY_BASE")
-config :links, :sendgrid_api_key, System.fetch_env!("SENDGRID_API_KEY")
+config :links, :mandrill_api_key, System.fetch_env!("MANDRILL_API_KEY")
+config :links, :mandrill_user_name, System.fetch_env!("MANDRILL_USERNAME")
